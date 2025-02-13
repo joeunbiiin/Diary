601d90852549f44b183ad4b14d0a85647f6bdc9d
joeunbiiin <110948574+joeunbiiin@users.noreply.github.com>
2025-02-13 17:53:36 +0900

    ver.1.1

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import { useReducer, useRef } from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Diary from "./pages/Diary";
@@ -6,9 +7,29 @@ import New from "./pages/New";
 import Edit from "./pages/Edit";
 import Notfound from "./pages/Notfound";
 
+const mockData = [
+  {
+    id: 1,
+    createdData: new Date().getTime(),
+    emotioId: 1,
+    content: "1번째 일기입니다 ^^~!",
+  },
+  {
+    id: 2,
+    createdData: new Date().getTime(),
+    emotioId: 2,
+    content: "2번째 일기입니다 ^^~!",
+  },
+];
+
 function App() {
+  const [data, dispatch] = useReducer(reducer, mockdata);
+  const idRef = useRef(3);
   return (
     <>
+      <button>일기 추가</button>
+      <button>일기 수정</button>
+      <button>일기 삭제</button>
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/new" element={<New />}></Route>
